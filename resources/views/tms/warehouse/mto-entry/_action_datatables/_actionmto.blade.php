41fde89316731decc8d5ac80628792c3aa300e50
Wildan Fathur Rohman/5156 <wildan@pttrimitra.com>
2020-11-09 15:30:01 +0700

    modify

--- resources/views/tms/warehouse/mto-entry/_action_datatables/_actionmto.blade.php
+++ resources/views/tms/warehouse/mto-entry/_action_datatables/_actionmto.blade.php
@@ -1,9 +1,9 @@
-<div style="text-align: center;">
-<a href="#" data-toggle="tooltip" row-id="{{ $model->id_mto }}"  data-placement="top"  title="View" class="view"> <i class="ti-eye"></i></a>
-<a href="#" data-toggle="tooltip" row-id="{{ $model->id_mto }}"  data-placement="top" title="Edit" class="edit"> <i class="ti-pencil-alt"></i></a>
-<a href="#" data-toggle="tooltip" row-id="{{ $model->id_mto }}" data-id={{ $model->mto_no }}  data-placement="top" title="Delete" class=" delete"> <i class="ti-trash"></i></a>
-<a href="{{ $url_print }}" data-toggle="tooltip" row-id=""  data-placement="top" title="Print" class="print"> <i class="fa fa-print"></i></a>
-<a href="#" data-toggle="tooltip" row-id="{{ $model->id_mto }}" data-id={{ $model->mto_no }}  data-placement="top" title="Posted" class=" posted"> <i class="fa fa-paper-plane"></i></a>
+<div style="text-align: center; ">
+<a href="#" style="color: black;" data-toggle="tooltip" row-id="{{ $model->id_mto }}"  data-placement="top"  title="View" class="view"> <i class="ti-eye"></i></a>
+<a href="#" style="color: black;" data-toggle="tooltip" row-id="{{ $model->id_mto }}"  data-placement="top" title="Edit" class="edit"> <i class="ti-pencil-alt"></i></a>
+<a href="#" style="color: black;" data-toggle="tooltip" row-id="{{ $model->id_mto }}" data-id={{ $model->mto_no }}  data-placement="top" title="Delete" class=" delete"> <i class="ti-trash"></i></a>
+<a href="{{ $url_print }}" style="color: black;" data-toggle="tooltip" row-id=""  data-placement="top" title="Print" class="print"> <i class="fa fa-print"></i></a>
+<a href="#" data-toggle="tooltip" style="color: black;" row-id="{{ $model->id_mto }}" data-id={{ $model->mto_no }}  data-placement="top" title="Posted" class=" posted"> <i class="fa fa-paper-plane"></i></a>
 </div>
 <script>
     $(function () {
