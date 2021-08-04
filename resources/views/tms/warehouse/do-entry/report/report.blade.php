2974bb9fd91e2ce87fa160a44db2e5a210cb9105
aldiabdumalik <aldiabdumalik@hotmail.com>
2021-08-04 14:29:25 +0700

    revisi print blank

--- resources/views/tms/warehouse/do-entry/report/report.blade.php
+++ resources/views/tms/warehouse/do-entry/report/report.blade.php
@@ -4,231 +4,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <style>
-        * {margin: 0;}
-        @page { size: 16.4cm 24.3cm landscape; }
-        body {
-            padding-left: 1.9cm;
-            padding-right: 1.9cm;
-            /* padding-top: 0.9cm; */
-        }
-        .sq-header {
-            width: 20.3cm;
-            height: 2.6cm;
-            padding: 0;
-            margin: 0;
-            border: 1px solid transparent;
-        }
-        .sq-no {
-            width: 10cm;
-            height: 2.6cm;
-            float: left;
-            border: none;
-            padding: 0;
-        }
-        .sq-cust span {
-            display: block;
-            font-weight: normal;
-        }
-        .sq-cust {
-            width: 10cm;
-            height: 2.6cm;
-            float: left;
-            padding: 0;
-            padding: 0 5px 0 5px;
-            border: 1px solid #000;
-            font-size: 12px;
-            font-weight: bold;
-        }
-        .sq-dgnhor {
-            height: 0.7cm;
-            width: 20.3cm;
-            font-size: 12px;
-            text-align: left;
-            padding: 0;
-            padding-bottom: 4px;
-            margin: 0;
-            border: none;
-        }
-        .sq-utama {
-            width: 20.3cm;
-            height: 10cm;
-            border: 1px solid #000;
-            padding: 0;
-            margin: 0;
-        }
-        .sq-1 {
-            width: 1.2cm; 
-            height: 7.8cm;
-            margin: 0;
-            padding: 0;
-            float: left;
-            border: none;
-            border-bottom: 1px solid #000;
-        }
-        .sq-2 {
-            width: 9.7cm; 
-            height: 7.8cm;
-            margin: 0;
-            padding: 0;
-            float: left;
-            border: 1px solid #000;
-            border-top: none;
-        }
-        .sq-3 {
-            width: 4.4cm; 
-            height: 7.8cm;
-            margin: 0;
-            padding: 0;
-            float: left;
-            border: none;
-            border-right: 1px solid #000;
-            border-bottom: 1px solid #000;
-        }
-        .sq-4 {
-            width: 4.9cm; 
-            height: 7.8cm;
-            margin: 0;
-            padding: 0;
-            float: left;
-            border: none;
-            border-bottom: 1px solid #000;
-        }
-        .sq-1-header {
-            width: 1.2cm;
-            height: 0.8cm;
-            margin: 0;
-            padding: 0;
-            text-align: center;
-            border: none;
-            border-bottom: 1px solid #000;
-        }
-        .sq-2-header {
-            width: 9.7cm;
-            height: 0.8cm;
-            margin: 0;
-            padding: 0;
-            text-align: center;
-            border: none;
-            border-bottom: 1px solid #000;
-        }
-        .sq-3-header {
-            width: 4.4cm;
-            height: 0.8cm;
-            margin: 0;
-            padding: 0;
-            text-align: center;
-            border: none;
-            border-bottom: 1px solid #000;
-        }
-        .sq-4-header {
-            width: 4.9cm;
-            height: 0.8cm;
-            margin: 0;
-            padding: 0;
-            text-align: center;
-            border: none;
-            border-bottom: 1px solid #000;
-        }
-        .footer {
-            width: 20.3cm;
-            height: auto;
-            position: absolute;
-            bottom: 3px;
-            font-size: 12px;
-            padding: 0;
-            margin: 0;
-            border: none;
-        }
-        .foot-1 {
-            display: inline-block;
-            overflow: auto;
-        }
-        .foot-2 {
-            display: inline-block;
-            float: right;
-            overflow: auto;
-        }
-        .sq-cops {
-            width: 20.3cm;
-            height: 2.2cm;
-            padding: 0;
-            margin: 0;
-            border: none;
-        }
-        .sq-cops1 {
-            width: 50%; 
-            height: 1.1cm;
-            padding: 0;
-            margin: 0;
-            padding-top: 0.5cm;
-            float: left;
-            border: none;
-            font-size: 13px;
-        }
-        .sq-cops2 {
-            width: 20.3cm;
-            height: 1.1cm;
-            padding: 0;
-            margin: 0;
-            border: none;
-            font-size: 16px;
-            text-align: center;
-            font-weight: bold;
-        }
-        .sq-ttd {
-            display: table;
-            width: 20.3cm;
-            height: 2.2cm;
-            padding: 0;
-            margin: 0;
-            position: absolute;
-            bottom: 26px;
-            border: none;
-        }
-        .sq-ttd-col {
-            display: table-cell;
-            width: 20%;
-            height: 2.2cm;
-            padding: 0;
-            margin: 0;
-            border: none;
-            /* border-right: 0.5px solid #000; */
-        }
-        .sq-ttd-col:last-child {
-            border-right: none;
-        }
-        .sq-ttd-isi {
-            font-size: 14px;
-            padding: 10px;
-            text-align: center;
-        }
-
-
-        .sq-no-row {
-            display: table;
-            width: 100%;
-            height: auto;
-        }
-        .sq-no-col-1 {
-            display: table-cell;
-            width: 20%;
-            text-align: left;
-            /* border: 0.1px solid blue; */
-        }
-        .sq-no-col-2 {
-            display: table-cell;
-            width: 5%;
-            text-align: left;
-            /* border: 0.1px solid blue; */
-        }
-        .sq-no-col-3 {
-            display: table-cell;
-            width: 50%;
-            text-align: left;
-            /* border: 0.1px solid blue; */
-        }
-    </style>
+    @include('tms.warehouse.do-entry.report.cssReport')
 </head>
 <body>
     <fieldset class="sq-cops">
@@ -296,10 +72,21 @@
             <div class="sq-2-header">
                 <p style="padding:3px;font-weight:bold;">NO. PART / NAMA BARANG</p>
             </div>
-            <div style="line-height: 20px;font-size: 12px;">
-                @foreach($groupItem[$getKey[$i]] as $item)
-                <div style="text-align: left;padding-left: 10px;">{{ $item->part_no }} / {{ $item->part_name }}</div>
-                @endforeach
+            <div class="sq-2-row">
+                <div class="sq-2-col">
+                    <div style="line-height: 20px;font-size: 12px;">
+                        @foreach($groupItem[$getKey[$i]] as $item)
+                        <div style="text-align: left;padding-left: 10px;">{{ $item->part_no }}</div>
+                        @endforeach
+                    </div>
+                </div>
+                <div class="sq-2-col">
+                    <div style="line-height: 20px;font-size: 12px;">
+                        @foreach($groupItem[$getKey[$i]] as $item)
+                        <div style="text-align: left;padding-left: 10px;">{{ $item->part_name }}</div>
+                        @endforeach
+                    </div>
+                </div>
             </div>
         </fieldset>
         <fieldset class="sq-3">
