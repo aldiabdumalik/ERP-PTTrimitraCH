e57032b185521b0cf08d530551181d7c68e4a05e
aldiabdumalik <aldiabdumalik@hotmail.com>
2021-08-31 10:15:05 +0700

    prepare project cust invoice

---
+++ app/Http/Controllers/Tms/Warehouse/CustInvoiceController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\TMS\Warehouse;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CustInvoiceController extends Controller
+{
+    //
+}
---
+++ app/Models/Dbtbs/CustInvoice.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Dbtbs;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CustInvoice extends Model
+{
+    //
+}
