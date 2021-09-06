b059fcd2ad2bbd2b41e3e775f532678311cfe5cf
aldiabdumalik <aldiabdumalik@hotmail.com>
2021-09-06 13:47:18 +0700

    pripare module customer price

---
+++ app/Models/Dbtbs/CustPrice.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models\Dbtbs;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class CustPrice extends Model
+{
+    use Notifiable;
+
+    protected $connection = 'db_tbs';
+    protected $table = 'entry_custprice_tbl';
+    protected $fillable = [
+        'id_price',
+        'status',
+        'cust_id',
+        'item_code',
+        'currency',
+        'price',
+        'active_date',
+    ];
+    protected $hidden = [];
+    public $timestamps = false;
+}
---
+++ resources/views/tms/warehouse/cust-price/index.blade.php
@@ -0,0 +1,130 @@
+@extends('master')
+@section('title', 'TMS | Warehouse - Customer Price')
+@section('css')
+<link rel="stylesheet" type="text/css" href="{{ asset('/vendor/Datatables/dataTables.bootstrap4.min.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('/vendor/datepicker/bootstrap-datepicker.min.css') }}">
+@endsection
+@section('content')
+
+@include('tms.warehouse.cust-price.style.custom-style')
+
+<div class="main-content-inner">
+    {{-- @include('tms.warehouse.cust-invoice.table.tableindex') --}}
+</div>
+{{-- @include('tms.warehouse.cust-invoice.modal.create.index') --}}
+
+@endsection
+@section('script')
+<script>
+    $(document).ready(function () {
+        const token_header = {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')};
+        const tbl_attr = (targets=[]) => {
+            const obj = {
+                destroy: true,
+                lengthChange: false,
+                searching: false,
+                paging: false,
+                ordering: false,
+                scrollY: "200px",
+                scrollCollapse: true,
+                fixedHeader: true,
+                columnDefs: [{
+                    targets: targets,
+                    createdCell:  function (td, cellData, rowData, row, col) {
+                        $(td).addClass('text-right');
+                    }
+                }]
+            };
+            return obj;
+        };
+        modalAction('#custinv-modal-index');
+
+        const index_data = new Promise(function(resolve, reject) {
+            let tbl_index = $('#custinv-datatables').DataTable();
+            resolve(tbl_index);
+        });
+
+        var tbl_item = $('#custinv-datatables-index').DataTable(tbl_attr([0,7,8,9]));
+        var tbl_item_part = $('#custinv-datatables-index-part').DataTable(tbl_attr([6,7,8]));
+
+        $('#custinv-modal-index').on('shown.bs.modal', function () {
+            tbl_item.columns.adjust().draw();
+            tbl_item_part.columns.adjust().draw();
+        });
+
+        $('#carouselExampleSlidesOnly').on('slid.bs.carousel', function () {
+            var idx = $(this).find('.active').index();
+            if (idx == 0) {
+                $('#custinv-text-view-by').text("VIEW BY DO NO.");
+            }else{
+                tbl_item_part.columns.adjust().draw();
+                $('#custinv-text-view-by').text("VIEW BY PART NO.");
+            }
+        })
+
+        function modalAction(elementId=null, action='show'){
+            return new Promise(resolve => {
+                $(elementId).modal(action);
+                resolve($(elementId));
+            });
+        }
+
+        function ajaxCall(params) {
+            return new Promise((resolve, reject) => {
+                $('body').loading({
+                    message: "wait for a moment...",
+                    zIndex: 9999
+                });
+                $.ajax({
+                    url: params.route,
+                    method: params.method,
+                    dataType: "JSON",
+                    cache: false,
+                    headers: token_header,
+                    data: params.data,
+                    error: function(response, status, x){
+                        Swal.fire({
+                            title: 'Access Denied',
+                            text: response.responseJSON.message,
+                            icon: 'error'
+                        });
+                        $('body').loading('stop');
+                        reject(response);
+                    },
+                    complete: function (response){
+                        $('body').loading('stop'); 
+                        resolve(response);
+                    }
+                });
+            });
+        }
+    });
+</script>
+@endsection
+@push('js')
+<script src="{{ asset('vendor/Datatables/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('vendor/Datatables/dataTables.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('vendor/datepicker/bootstrap-datepicker.min.js') }}"></script>
+<script src="{{ asset('vendor/jqloading/jquery.loading.min.js') }}"></script>
+<script>
+    $('.this-datepicker').datepicker({
+        format: 'dd/mm/yyyy',
+        autoclose: true,
+    }).datepicker("setDate",'now');
+    $("input[type=number]").on("input", function() {
+        var nonNumReg = /[^0-9.]/g
+        $(this).val($(this).val().replace(nonNumReg, ''));
+    });
+    @if(\Session::has('message'))
+    setTimeout(function () {
+        Swal.fire({
+            title: 'Warning!',
+            text: "{{\Session::get('message')}}",
+            icon: 'warning'
+        }).then(function () {
+            window.close();
+        });
+    }, 1000);
+    @endif
+</script>
+@endpush
