a4319299a10ded89183ae97797746ae11f42292c
aldiabdumalik <aldiabdumalik@hotmail.com>
2022-01-10 07:35:12 +0700

    update custprice fixed trait

--- app/Http/Traits/TMS/AR/CustPriceTrait.php
+++ app/Http/Traits/TMS/AR/CustPriceTrait.php
@@ -252,63 +252,53 @@ trait CustPriceTrait {
                         }
                         // End SSO
                         // Start SJ
-                        if ($data[$x]['is_sso'] == 1) {
-                            $sso_tch = DB::table('tch_tbs.sso_dtl as sso_dtl')
-                                ->leftJoin('tch_tbs.sso_hdr as sso_hdr', 'sso_hdr.sso_no', '=', 'sso_dtl.sso_no')
-                                ->leftJoin('tch_tbs.do_dtl as sj_dtl', function ($join){
-                                    $join->on('sj_dtl.sso_no', '=', 'sso_dtl.sso_no');
-                                    $join->on('sj_dtl.itemcode', '=', 'sso_dtl.itemcode');
-                                })
+                        if ($data[$x]['is_sj'] == 1) {
+                            $do_tch = DB::table('tch_tbs.do_dtl as sj_dtl')
                                 ->leftJoin('tch_tbs.do_hdr as sj_hdr', 'sj_hdr.do_no', '=', 'sj_dtl.do_no')
                                 ->leftJoin('tch_tbs.inv_sj as inv_dtl', function ($join){
                                     $join->on('inv_dtl.do_no', '=', 'sj_dtl.do_no');
                                 })
                                 ->where(function ($wh) use ($so_header, $item_i, $act_date){
-                                    $wh->where('sso_dtl.so_no', $so_header);
-                                    $wh->where('sso_dtl.itemcode', $item_i);
-                                    $wh->where('sso_dtl.written', '>=', $act_date);
+                                    $wh->where('sj_dtl.so_no', $so_header);
+                                    $wh->where('sj_dtl.itemcode', $item_i);
+                                    $wh->where('sj_dtl.written', '>=', $act_date);
                                     $wh->whereNull('inv_dtl.do_no');
                                 })
                                 ->select([
-                                    'sso_dtl.so_no',
-                                    'sso_hdr.period as sso_period',
-                                    'sso_hdr.taxrate as sso_tax_rate',
-                                    'sso_dtl.itemcode as sso_item_code',
-                                    'sso_dtl.price as sso_price',
-                                    'sso_dtl.quantity as sso_qty_sso',
-                                    'sso_hdr.sub_amt as sso_sub_amount',
-                                    'sso_hdr.tot_disc as sso_tot_vat',
-                                    'sso_hdr.tot_amt as sso_total_amount',
+                                    'sj_dtl.do_no',
+                                    'sj_hdr.period as sj_period',
+                                    'sj_hdr.taxrate as sj_tax_rate',
+                                    'sj_dtl.itemcode as sj_item_code',
+                                    'sj_dtl.price as sj_price',
+                                    'sj_dtl.quantity as sj_qty',
+                                    'sj_hdr.sub_amt as sj_sub_amount',
+                                    'sj_hdr.tot_disc as sj_tot_vat',
+                                    'sj_hdr.tot_amt as sj_total_amount',
                                     'inv_dtl.do_no as inv_do'
                                 ])
                                 ->get();
-                            if ($sso_tch->isNotEmpty()) {
-                                foreach ($sso_tch as $s) {
-                                    $sub_amt = $price_new * $s->sso_qty_sso;
-                                    $tot_vat = $sub_amt * $s->sso_tax_rate / 100;
+                            if ($do_tch->isNotEmpty()) {
+                                foreach ($do_tch as $s) {
+                                    $sub_amt = $price_new * $s->sj_qty;
+                                    $tot_vat = $sub_amt * $s->sj_tax_rate / 100;
                                     $total_amount = $sub_amt + $tot_vat;
-                                    DB::table('tch_tbs.sso_dtl as sso_dtl')
-                                        ->leftJoin('tch_tbs.sso_hdr as sso_hdr', 'sso_hdr.sso_no', '=', 'sso_dtl.sso_no')
-                                        ->leftJoin('tch_tbs.do_dtl as sj_dtl', function ($join){
-                                            $join->on('sj_dtl.sso_no', '=', 'sso_dtl.sso_no');
-                                            $join->on('sj_dtl.itemcode', '=', 'sso_dtl.itemcode');
-                                        })
+                                    DB::table('tch_tbs.do_dtl as sj_dtl')
                                         ->leftJoin('tch_tbs.do_hdr as sj_hdr', 'sj_hdr.do_no', '=', 'sj_dtl.do_no')
                                         ->leftJoin('tch_tbs.inv_sj as inv_dtl', function ($join){
                                             $join->on('inv_dtl.do_no', '=', 'sj_dtl.do_no');
                                         })
                                         ->where(function ($wh) use ($so_header, $item_i, $act_date){
-                                            $wh->where('sso_dtl.so_no', $so_header);
-                                            $wh->where('sso_dtl.itemcode', $item_i);
-                                            $wh->where('sso_dtl.written', '>=', $act_date);
+                                            $wh->where('sj_dtl.so_no', $so_header);
+                                            $wh->where('sj_dtl.itemcode', $item_i);
+                                            $wh->where('sj_dtl.written', '>=', $act_date);
                                             $wh->whereNull('inv_dtl.do_no');
                                         })
-                                        ->update([
-                                            'sso_dtl.price' => $price_new,
-                                            'sso_hdr.sub_amt' => $sub_amt,
-                                            'sso_hdr.tot_disc' => $tot_vat,
-                                            'sso_hdr.tot_amt' => $total_amount
-                                        ]);
+                                    ->update([
+                                        'sj_dtl.price' => $price_new,
+                                        'sj_hdr.sub_amt' => $sub_amt,
+                                        'sj_hdr.tot_disc' => $tot_vat,
+                                        'sj_hdr.tot_amt' => $total_amount
+                                    ]);
                                 }
                             }
                         }
